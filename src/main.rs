3c6d30a02536d56e80266c5d4fb5bbe9893b17e3
akthrms <akt.hrms@gmail.com>
2021-01-07 22:07:46 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
+use std::str::FromStr;
 use std::{env, process};
 
 mod tcp_client;
 mod tcp_server;
 mod udp_client;
 mod udp_server;
+
+enum Protocol {
+    Tcp,
+    Udp,
+}
+
+impl FromStr for Protocol {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "tcp" => Ok(Self::Tcp),
+            "udp" => Ok(Self::Udp),
+            _ => Err("Please specify tcp or udp on the 1st argument.".to_string()),
+        }
+    }
+}
+
+enum Role {
+    Server,
+    Client,
+}
+
+impl FromStr for Role {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "server" => Ok(Self::Server),
+            "client" => Ok(Self::Client),
+            _ => Err("Please specify server or client on the 2nd argument.".to_string()),
+        }
+    }
+}
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
@@ -14,41 +49,33 @@ fn main() {
         log::error!("Please specify [tcp|udp] [server|client] [addr:port].");
         process::exit(1);
     }
-    let protocol: &str = &args[1];
-    let role: &str = &args[2];
+
+    let protocol = args[1].parse::<Protocol>().unwrap_or_else(|e| {
+        log::error!("{}", &e);
+        process::exit(1);
+    });
+    let role = args[2].parse::<Role>().unwrap_or_else(|e| {
+        log::error!("{}", &e);
+        process::exit(1);
+    });
     let address: &str = &args[3];
 
     match protocol {
-        "tcp" => match role {
-            "server" => {
+        Protocol::Tcp => match role {
+            Role::Server => {
                 tcp_server::serve(address).unwrap_or_else(|e| log::error!("{}", e));
             }
-            "client" => {
+            Role::Client => {
                 tcp_client::connect(address).unwrap_or_else(|e| log::error!("{}", e));
             }
-            _ => {
-                missing_role();
-            }
         },
-        "udp" => match role {
-            "server" => {
+        Protocol::Udp => match role {
+            Role::Server => {
                 udp_server::serve(address).unwrap_or_else(|e| log::error!("{}", e));
             }
-            "client" => {
+            Role::Client => {
                 udp_client::communicate(address).unwrap_or_else(|e| log::error!("{}", e));
             }
-            _ => {
-                missing_role();
-            }
         },
-        _ => {
-            log::error!("Please specify tcp or udp on the 1st argument.");
-            process::exit(1);
-        }
     }
-}
-
-fn missing_role() {
-    log::error!("Please specify server or client on the 2nd argument.");
-    process::exit(1);
 }
